01431070789868fc665da7cdfd309357e6c87b0a
Laia Navalon <laianavalon@gmail.com>
2024-03-07 11:04:11 +0100

    Implement signup page and functionality

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,3 +1,81 @@
-function Signup() {}
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import authService from "../services/auth.service";
+
+const API_URL = "http://localhost:3000";
+
+function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+  const handleUsername = (e) => setUsername(e.target.value);
+
+  const handleSignupSubmit = (e) => {
+    e.preventDefault();
+    // Create an object representing the request body
+    const requestBody = { email, password, username };
+
+    // Make an axios request to the API
+    // If the POST request is a successful redirect to the login page
+    // If the request resolves with an error, set the error message in the state
+    authService
+      .signup(requestBody)
+      .then((response) => {
+        navigate("/login");
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        setErrorMessage(errorDescription);
+      });
+  };
+
+  return (
+    <div className="SignupPage">
+      <h1>Sign Up</h1>
+
+      <form onSubmit={handleSignupSubmit}>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmail}
+        />
+
+        <label>Username:</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Username"
+          value={username}
+          onChange={handleUsername}
+        />
+
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePassword}
+        />
+
+        <button type="submit">Sign Up</button>
+      </form>
+
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      <p>Already have account?</p>
+      <Link to={"/login"}> Login</Link>
+    </div>
+  );
+}
 
 export default Signup;
