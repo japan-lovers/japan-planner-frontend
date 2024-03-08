880f4c29e398d019c8e6e1d3e6b2a6baf84c4ee3
Thomas-Deblay <thomasdelay.dev@gmail.com>
2024-03-08 12:27:48 +0100

    remove the comments

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -45,35 +45,6 @@ function Login() {
   };
 
   return (
-    // <div className="flex flex-col">
-    //   <h1>Login</h1>
-
-    //   <form onSubmit={handleLoginSubmit} className="flex flex-col">
-    //     <label>Email:</label>
-    //     <input
-    //       type="email"
-    //       name="email"
-    //       placeholder="Email"
-    //       value={email}
-    //       onChange={handleEmail}
-    //     />
-
-    //     <label>Password:</label>
-    //     <input
-    //       type="password"
-    //       name="password"
-    //       placeholder="Password"
-    //       value={password}
-    //       onChange={handlePassword}
-    //     />
-
-    //     <button type="submit">Login</button>
-    //   </form>
-    //   {errorMessage && <p className="error-message">{errorMessage}</p>}
-
-    //   <p>Don't have an account yet?</p>
-    //   <Link to={"/signup"}> Sign Up</Link>
-    // </div>
     <div className="flex justify-center items-center mt-20">
       <Card color="transparent" shadow={false}>
         <Typography variant="h4" color="blue-gray">
