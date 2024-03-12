2917f9e1af11a12e856b87e2b4b2e31a20916711
Laia Navalon <laianavalon@gmail.com>
2024-03-12 10:14:55 +0100

    Fix add to favourites functionality

--- src/components/CardActivity.jsx
+++ src/components/CardActivity.jsx
@@ -26,7 +26,12 @@ function CardActivity({ activity }) {
       .getUser(user._id)
       .then((response) => {
         setFavourites(response.data.favouriteActivities);
-
+        response.data.favouriteActivities.forEach((elm) => {
+          if (elm._id === activity._id) {
+            setIsFavourite(true);
+            return;
+          }
+          return;
         });
       })
       .catch((error) => console.log(error));
@@ -67,14 +72,14 @@ function CardActivity({ activity }) {
           <IconButton
             onClick={handleAddToFavourites}
             size="sm"
-            color="black"
+            color="white"
             variant="text"
             className="!absolute top-2 right-2 rounded-full"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              // fill="currentColor"
+              fill="currentColor"
               className="h-6 w-6"
             >
               <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
@@ -84,14 +89,14 @@ function CardActivity({ activity }) {
           <IconButton
             onClick={handleAddToFavourites}
             size="sm"
-            color="red"
+            color="white"
             variant="text"
             className="!absolute top-2 right-2 rounded-full"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              fill="currentColor"
+              // fill="currentColor"
               className="h-6 w-6"
             >
               <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
