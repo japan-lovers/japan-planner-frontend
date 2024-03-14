f1e17b0c5e1d36aab0a00ba6ebfb3bb8e60fb214
Thomas-Deblay <thomasdelay.dev@gmail.com>
2024-03-14 14:30:33 +0100

    css changes and started implementing map

--- src/components/CardTrips.jsx
+++ src/components/CardTrips.jsx
@@ -15,20 +15,20 @@ function CardTrips({ tripData }) {
   return (
     <div>
       <Link to={`/trips/${tripData._id}`}>
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8 ">
+          <div className="flex flex-col mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none ">
             <div className="p-8 sm:p-10 lg:flex-auto">
               <h3 className="text-2xl font-bold tracking-tight text-gray-900">
                 {tripData.destinations}
               </h3>
 
-              <p className="mt-6 text-base leading-7 text-gray-600">
+              {/* <p className="mt-6 text-base leading-7 text-gray-600">
                 Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque
                 amet indis perferendis blanditiis repellendus etur quidem
                 assumenda.
-              </p>
+              </p> */}
             </div>
-            <div className="flex mt-2 mb-2 mr-2 overflow-x-auto">
+            <div className="flex mt-2 mb-2  overflow-x-auto ml-8 mr-8 ">
               {tripData.activities.map((el) => (
                 <div className="z-1  -mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink flex ">
                   <div
