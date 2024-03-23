a0d3b81e70fbd7ec9bbd3fb7cf8b6100f9274fa4
Thomas-Deblay <thomasdelay.dev@gmail.com>
2024-03-23 01:02:18 +0100

    dark mode fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 
-const withMT = require("@material-tailwind/react/utils/withMT");
+const withMT = require('@material-tailwind/react/utils/withMT');
 
 module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       spacing: {
-        tripform: "900px",
-        profile: "600px",
-        minusminus: "-180px",
-        minus: "-130px",
-        minus50: "-50px",
-        minus40: "-40px",
-        card: "290px",
-        imgw: "600px",
-        imgh: "450px",
-        maxw: "1260px",
-        45: "47%",
-        30: "30%",
-        22: "22%",
+        tripform: '900px',
+        profile: '600px',
+        minusminus: '-180px',
+        minus: '-130px',
+        minus50: '-50px',
+        minus40: '-40px',
+        card: '290px',
+        imgw: '600px',
+        imgh: '450px',
+        maxw: '1260px',
+        45: '47%',
+        30: '30%',
+        22: '22%',
       },
     },
     colors: {
-      modalbg: "rgba(0, 0, 0, 0.5)",
+      modalbg: 'rgba(0, 0, 0, 0.5)',
     },
     backgroundImage: {
       home: "url('/public/japan.jpg')",
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')],
+  darkMode: false,
+  daisyui: {
+    darkTheme: false,
+  },
 });
