6a7ebc40d9f8046e181ed2f2438b02fbe427da38
Laia Navalon <laianavalon@gmail.com>
2024-03-25 13:04:47 +0100

    Create footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import ReactDOM from 'react-dom';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
 
-import Homepage from './pages/Homepage';
-import About from './pages/About';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import UserProfile from './pages/UserProfile';
-import AllTrips from './pages/AllTrips';
-import TripDetails from './pages/TripDetails';
-import AllActivities from './pages/AllActivities';
-import ActivityDetails from './pages/ActivityDetails';
-import Navbar from './components/Navbar';
+import Homepage from "./pages/Homepage";
+import Footer from "./components/Footer";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import UserProfile from "./pages/UserProfile";
+import AllTrips from "./pages/AllTrips";
+import TripDetails from "./pages/TripDetails";
+import AllActivities from "./pages/AllActivities";
+import ActivityDetails from "./pages/ActivityDetails";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
@@ -28,6 +30,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
+
+      <Footer />
     </div>
   );
 }
