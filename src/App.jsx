5ef163593d08d205c3bc63a97ba823250f09c0ea
Laia Navalon <laianavalon@gmail.com>
2024-06-13 14:21:49 +0200

    Create routing

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import TripDetails from "./pages/TripDetails";
 import AllActivities from "./pages/AllActivities";
 import ActivityDetails from "./pages/ActivityDetails";
 import Navbar from "./components/Navbar";
+import ChangeImage from "./pages/ChangeImage";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Route path="/activities/:id" element={<ActivityDetails />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/image" element={<ChangeImage />} />
       </Routes>
 
       <Footer />
