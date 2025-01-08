c24a502375e56c75c386b820b29b047c3c8ff790
Laia Navalon <laianavalon@gmail.com>
2025-01-08 20:36:27 +0100

    Remove ChangeImage route

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ function App() {
         <Route path="/activities/:id" element={<ActivityDetails />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/image" element={<ChangeImage />} />
       </Routes>
 
       <Footer />
